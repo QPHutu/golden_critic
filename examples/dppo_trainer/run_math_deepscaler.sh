008fe5c570f2e744686ef2f35adc86c512aee288
Penghui Qi <qph0703@126.com>
2026-07-19 17:12:47 +0800

    deepscaler for R1D

--- examples/dppo_trainer/run_math_deepscaler.sh
+++ examples/dppo_trainer/run_math_deepscaler.sh
@@ -11,14 +11,14 @@ INFER_BACKEND=${INFER_BACKEND:-vllm}
 
 # Paths
 DATA_ROOT=${DATA_ROOT:-"/home/aiops/qiph/verl"}
-MODEL_PATH=${MODEL_PATH:-"${DATA_ROOT}/models/Qwen3-8B-Base"}
+MODEL_PATH=${MODEL_PATH:-"${DATA_ROOT}/models/DeepSeek-R1-Distill-Qwen-1.5B"}
 
 CRITIC_MODEL_PATH=${CRITIC_MODEL_PATH:-$MODEL_PATH}
 NNODES=${NNODES:-2}
 NDEVICES_PER_NODE=${NDEVICES_PER_NODE:-8}
 
-TRAIN_BATCH_SIZE=${TRAIN_BATCH_SIZE:-128}
-PPO_MINI_BATCH_SIZE=${PPO_MINI_BATCH_SIZE:-32}
+TRAIN_BATCH_SIZE=${TRAIN_BATCH_SIZE:-1024}
+PPO_MINI_BATCH_SIZE=${PPO_MINI_BATCH_SIZE:-256}
 MAX_PROMPT_LENGTH=${MAX_PROMPT_LENGTH:-6000}
 MAX_RESPONSE_LENGTH=${MAX_RESPONSE_LENGTH:-24000}
 PPO_MAX_TOKEN_LEN_PER_GPU=${PPO_MAX_TOKEN_LEN_PER_GPU:-30000}
@@ -28,22 +28,28 @@ CRITIC_LR=${CRITIC_LR:-5e-6}
 ENTROPY_COEFF=${ENTROPY_COEFF:-0}
 
 ROLLOUT_TP=${ROLLOUT_TP:-2}
-ROLLOUT_GPU_MEM_UTIL=${ROLLOUT_GPU_MEM_UTIL:-0.6}
+ROLLOUT_GPU_MEM_UTIL=${ROLLOUT_GPU_MEM_UTIL:-0.75}
 ROLLOUT_N=${ROLLOUT_N:-1}
-ROLLOUT_VAL_N=${ROLLOUT_VAL_N:-4}
+ROLLOUT_VAL_N=${ROLLOUT_VAL_N:-32}
 
 TOTAL_EPOCHS=${TOTAL_EPOCHS:-1500}
-SAVE_FREQ=${SAVE_FREQ:-20000}
-TEST_FREQ=${TEST_FREQ:-20}
+SAVE_FREQ=${SAVE_FREQ:-250}
+TEST_FREQ=${TEST_FREQ:-25}
 
 CRITIC_KEY=${CRITIC_KEY:-""}
 ALGO=${ALGO:-"dppo_tv"}
 AGG_MODE=${AGG_MODE:-"seq-mean-token-sum"}
-LAM=${LAM:-1.0}
-ADV_ESTIMATOR=${ADV_ESTIMATOR:-"gae"}
+LAM=${LAM:-0.4}
+ADV_ESTIMATOR=${ADV_ESTIMATOR:-"lagae"}
+
+if [[ $ADV_ESTIMATOR == "grpo" ]]; then
+    TRAIN_BATCH_SIZE=64
+    PPO_MINI_BATCH_SIZE=16
+    ROLLOUT_N=16
+fi
 
 PROJECT_NAME=${PROJECT_NAME:-verl_ppo_deepscaler}
-EXPERIMENT_NAME=${EXPERIMENT_NAME:-adv_ret_bdv_${ADV_ESTIMATOR}_${ALGO}_${AGG_MODE}_${CRITIC_KEY}_Lam${LAM}_$(date +%Y%m%d_%H%M)}
+EXPERIMENT_NAME=${EXPERIMENT_NAME:-${CRITIC_KEY}_${ADV_ESTIMATOR}_Lam${LAM}_${ALGO}_${AGG_MODE}_$(date +%Y%m%d_%H%M)}
 
 # GSM8K_TRAIN_FILE=${GSM8K_TRAIN_FILE:-$HOME/data/gsm8k/train.parquet}
 # GSM8K_TEST_FILE=${GSM8K_TEST_FILE:-$HOME/data/gsm8k/test.parquet}
@@ -53,7 +59,7 @@ EXPERIMENT_NAME=${EXPERIMENT_NAME:-adv_ret_bdv_${ADV_ESTIMATOR}_${ALGO}_${AGG_MO
 CKPTS_DIR=${CKPTS_DIR:-"${DATA_ROOT}/ckpts/${PROJECT_NAME}/${EXPERIMENT_NAME}"}
 ROOT="${DATA_ROOT}/data/deepscaler/"
 MATH_TRAIN_FILE=${TRAIN_FILE:-"${ROOT}/deepscaler.parquet"}
-MATH_TEST_FILE=${TEST_FILE:-"[${ROOT}/aime_2025.parquet,${ROOT}/aime_2026.parquet,${ROOT}/brumo_2025.parquet,${ROOT}/hmmt_feb_2026.parquet,${ROOT}/hmmt_nov_2025.parquet]"}
+MATH_TEST_FILE=${TEST_FILE:-"[${ROOT}/aime_2025.parquet]"}
 # MATH_TEST_FILE=${TEST_FILE:-"[${ROOT}/aime_2025.parquet,${ROOT}/aime_2026.parquet,${ROOT}/brumo_2025.parquet,${ROOT}/hmmt_feb_2026.parquet,${ROOT}/hmmt_nov_2025.parquet]"}
 ########################### end user-adjustable ###########################
 
@@ -90,6 +96,7 @@ ACTOR=(
     actor_rollout_ref.actor.use_dynamic_bsz=True
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${PPO_MAX_TOKEN_LEN_PER_GPU}
     actor_rollout_ref.actor.entropy_coeff=${ENTROPY_COEFF}
+    actor_rollout_ref.actor.calculate_entropy=True
     actor_rollout_ref.actor.fsdp_config.param_offload=True
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True
 )
@@ -126,7 +133,7 @@ CRITIC=(
 
 TRAINER=(
     trainer.balance_batch=True
-    trainer.critic_warmup=10
+    trainer.critic_warmup=15
     trainer.log_val_generations=1
     trainer.logger='["console","wandb"]'
     trainer.project_name=${PROJECT_NAME}
